e83e1319458036a1cc5b27ce44e25e2b2648c95e
Алексей Шербаков <leha.gb.2023@mail.ru>
2024-09-22 14:29:08 +1000

    Работа с массивом

---
+++ Trash/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trash
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            Console.Write("ВВедите размер массива: ");
+            int num;
+            bool n = int.TryParse(Console.ReadLine(), out num);
+
+            int[] arr = new int[num];
+
+            Console.WriteLine("Исходный массив: ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(0, 100);
+                Console.Write($"{arr[i]} ");
+            }
+            int index = arr[0];
+
+            Console.WriteLine("\nРезультат: ");
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if(index > arr[i + 1])
+                {
+                    int temp = index;
+                    index = arr[i + 1];
+                    arr[i + 1] = temp;
+                }
+                else
+                {
+                    continue;
+                }
+                Console.Write($"{arr[i]} ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Trash/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trash
+{
+    class Program
+    {
+        static Random rnd = new Random();
+        static int[] arr;
+        static void Main(string[] args)
+        {
+            Print(arr);
+        }
+        private static int[] FillingArray()
+        {
+            Console.Write("Введите размер массива: ");
+            int n = int.Parse(Console.ReadLine());
+          
+            arr = new int[n];
+            Console.WriteLine("Исходный массив: ");
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = rnd.Next(0, 101);
+                Console.Write($"{arr[i]} ");
+            }
+            return arr;
+        }
+        private static void Print(int[] arr)
+        {
+            arr = FillingArray();
+            Console.WriteLine("\n");
+            Console.WriteLine("Упорядоченный массив: ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        int temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+                Console.Write($"{arr[i]} ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
