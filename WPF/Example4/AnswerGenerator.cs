eccf752d87be83118c5ca6c886ad252958459898
Алексей Шербаков <leha.gb.2023@mail.ru>
2024-09-28 16:37:09 +1000

    Урок № 1

---
+++ WPF/Example4/AnswerGenerator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example4
+{
+    public static class AnswerGenerator
+    {
+        private static string[] answers = new string[]
+        {
+            "Спроси еще раз.",
+            "Не могу сразу сказать.",
+            "Без сомнений.",
+            "Это действительно так.",
+            "Сосредоточся и спроси еще раз.",
+            "Нет.",
+            "Да, действительно.",
+            "Я так не считаю.",
+            "Именно так.",
+            "Лучше не говори ничего.",
+            "Плохая точка зрения.",
+            "Вероятно это так.",
+            "Очень сомнительно.",
+            "Как я вижу, то да.",
+            "Мой ответ, нет.",
+            "Конечно.",
+            "Да.",
+            "Можете быть уверены.",
+            "Хорошая точка зрения.",
+            "Смутный ответ, попробуй снова."
+        };
+
+
+        // Метод случайной генерации ответа.
+        public static string GetRandomAnswer()
+        {
+            return answers[new Random().Next(answers.Length)];
+        }
+    }
+}
---
+++ WPF/Example4/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Example4
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Answer_Click(object sender, RoutedEventArgs e)
+        {
+            // Ставим курсор ожидания.
+            this.Cursor = Cursors.Wait;
+
+            // Делаем задержку, для создания эффекта, что программа задумалась.
+            Thread.Sleep(1000);
+
+            // Берем случайный ответ.
+            txtAnswer.Text = AnswerGenerator.GetRandomAnswer();
+
+            // Восстонавиваем прежнее состояние курсора.
+            Cursor = null;
+        }
+    }
+}
