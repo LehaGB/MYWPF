573dec4f6e60c6bfc25eb4197d29b232657fbacb
Алексей Шербаков <leha.gb.2023@mail.ru>
2024-10-03 18:18:28 +1000

    Интерфейс

---
+++ Module10/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            User user = new User();
+            user.Read();
+            Console.ReadLine();
+        }
+    }
+    interface IUser
+    {
+        void Read();
+    }
+
+    interface IAdmin : IUser
+    {
+        void Write();
+    }
+    public class User : IUser
+    {
+        public void Read()
+        {
+            Console.WriteLine("User is reading...");
+        }
+    }
+    public class Admin : IAdmin
+    {
+        public void Read()
+        {
+            Console.WriteLine("Admin is reading...");
+        }
+
+        public void Write()
+        {
+            Console.WriteLine("Admin is writing...");
+        }
+    }
+}
