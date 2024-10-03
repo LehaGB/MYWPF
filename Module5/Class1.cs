20396b4fab625db811214ad22154cf38b158aba5
Алексей Шербаков <leha.gb.2023@mail.ru>
2024-10-03 18:19:50 +1000

    Повтор с 5-го домашнего задания

---
+++ Module5/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module5
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите предложение: ");
+            string text = Console.ReadLine();
+
+            ReversWords(text);
+
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// Разбиваем предложение на пробелы.
+        /// </summary>
+        /// <param name="inputPhrase">Входящее предложение</param>
+        static void ReversWords(string inputPhrase)
+        {
+            string[] text = inputPhrase.Split(' ');
+            string result = Reverse(inputPhrase);
+            Console.Write($"Перевернутое предложение: {result}");
+
+        }
+
+        /// <summary>
+        /// Разворачиваем предложение в обратной последовательности.
+        /// </summary>
+        /// <param name="text">Входящее предложение</param>
+        /// <returns>Развернутое предложение</returns>
+        static string Reverse(string text)
+        {
+            string reversed = String.Empty;
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                reversed += text[i];
+            }
+            return reversed;
+        }
+    }
+}
---
+++ Module5/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите предложение: ");
+            string text = Console.ReadLine();
+
+            Console.WriteLine();
+
+            SplitText(text);
+            Print(text);
+
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// Разбиваем предложение на пробелы.
+        /// </summary>
+        /// <param name="text">Входящее предложение</param>
+        /// <returns>Предложение разбитое на пробелы</returns>
+        static string[] SplitText(string text)
+        {
+            string[] subs = text.Split(' ');
+            return subs;
+        }
+
+        /// <summary>
+        /// Распечатываем каждое слово предложения с новой строки.
+        /// </summary>
+        /// <param name="text">Входящее предложение</param>
+        static void Print(string text)
+        {
+            string[] newString = SplitText(text);
+            foreach (var e in newString)
+            {
+                Console.WriteLine($"{e}");
+            }
+
+        }
+    }
+}
